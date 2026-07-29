1237e673ca5dadeb921da3a2eef9d943bd2698e5
69070157-afk <69070157@kmitl.ac.th>
2026-07-29 21:48:44 +0700

    Add OJ3072 vowel counter and submission log
    Add oj3072/code.py: a simple case-insensitive vowel counter that reads input, counts a/e/i/o/u and prints each vowel count if >0. Add oj3072/submission.md: student learning log (Thai) documenting the solution, tests and OJ submission details (OJ3072, submission ID 575254, Pass).

---
+++ oj3072/code.py
@@ -0,0 +1,20 @@
+'''PEP P '''
+
+txt = input().lower()
+
+a = int(txt.count('a'))
+e = int(txt.count('e'))
+i = int(txt.count('i'))
+o = int(txt.count('o'))
+u = int(txt.count('u'))
+
+if a :
+    print('a :',a)
+if e :
+    print('e :',e)
+if i :
+    print('i :',i)
+if o :
+    print('o :',o)
+if u :
+    print('u :',u)
